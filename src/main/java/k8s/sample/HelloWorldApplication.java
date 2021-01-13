324aaec833f175754c2c8f32dfa54bae25783e6f
amoravek <amoravek@gmail.com>
2021-01-14 00:23:52 +0100

    Skeleton

---
+++ src/main/java/k8s/sample/HelloWorldApplication.java
@@ -0,0 +1,32 @@
+package k8s.sample;
+
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+import k8s.sample.health.TemplateHealthCheck;
+import k8s.sample.resources.HelloWorldResource;
+
+public class HelloWorldApplication extends Application<HelloWorldConfiguration> {
+	public static void main(String[] args) throws Exception {
+		new HelloWorldApplication().run(args);
+	}
+
+	@Override
+	public String getName() {
+		return "hello-world";
+	}
+
+	@Override
+	public void initialize(Bootstrap<HelloWorldConfiguration> bootstrap) {
+		// nothing to do yet
+	}
+
+	@Override
+	public void run(HelloWorldConfiguration configuration, Environment environment) {
+		final HelloWorldResource resource = new HelloWorldResource(configuration.getTemplate(), configuration.getDefaultName());
+		final TemplateHealthCheck healthCheck = new TemplateHealthCheck(configuration.getTemplate());
+		environment.healthChecks().register("template", healthCheck);
+		environment.jersey().register(resource);
+	}
+
+}
